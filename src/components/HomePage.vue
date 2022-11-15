baba33336ad67c724b997eb9cd747644c32c4404
Tyler Popson <tylerpopson@gmail.com>
2022-11-15 16:53:59 -0500

    added background and scroll

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -22,18 +22,22 @@ document.addEventListener("mousemove", mouse);
 document.addEventListener("touchmove", mouse);
 </script>
 <template>
-  <div class="flex fixed">
+  <div class="static w-screen h-screen">
+  <div class="flex bg-black">
     <div
       id="top"
-      class="flex flex-none w-1/2 h-screen bg-purple-600 fixed top-0 left-0 z-10 overflow-hidden place-items-center"
+      class="flex flex-none w-1/2 h-screen bg-spiral-background fixed top-0 left-0 z-10 overflow-hidden place-items-center bg-fixed"
     >
-      <p class="w-screen text-center absolute overflow-hidden">top</p>
+      <!-- <div class="w-screen text-center absolute overflow-hidden">top</div> -->
     </div>
     <div
       id="bottom"
-      class="flex w-screen h-screen bg-white fixed top-0 right-0 z-0 items-center justify-center"
+      class="flex w-screen h-screen bg-spiral-background grayscale fixed top-0 right-0 z-0 items-center justify-center bg-fixed"
     >
-      <p>bottom</p>
+      <!-- <div>bottom</div> -->
     </div>
+  </div>
+  </div>
+  <div class=" h-screen bg-black">
   </div>
 </template>
